6c4f936f0dc95a71080c45ce67a55d0a31b79d04
Adheesh Shenoy <adheeshenoy@gmail.com>
2022-11-29 21:15:20 -0800

    tensorflow code and baseline results

--- data_processing.py
+++ data_processing.py
@@ -1,42 +1,11 @@
 import tarfile
+import numpy as np
+import cv2
 import os
-import json
-
 
 def extract(path = 'dataset.tar'):
     file = tarfile.TarFile.open(path)
     file.extractall()
-    
-# def rename_folders(path = 'imagenette2'):
-#     mapping = dict(
-#     n01440764='tench',
-#     n02102040='English springer',
-#     n02979186='cassette player',
-#     n03000684='chain saw',
-#     n03028079='church',
-#     n03394916='French horn',
-#     n03417042='garbage truck',
-#     n03425413='gas pump',
-#     n03445777='golf ball',
-#     n03888257='parachute'
-#     )
-    
-#     os.rename(os.path.join(path, 'val'), os.path.join(path, 'test'))
-    
-#     info = {'train': {}, 'test': {}}
-    
-#     for folder in ('train', 'test'):
-#         folder_path = os.path.join(path, folder)
-#         for k, v in mapping.items():
-#             os.rename(os.path.join(folder_path, k), os.path.join(folder_path, v))
-            
-#             info[folder][v] = len(os.listdir(os.path.join(folder_path, v)))
-
-#         info[folder]['total'] = sum(info[folder].values())
-
-#     with open('data_description.json', 'w') as f:
-#         json.dump(info, f)
 
 if __name__ == '__main__':
     extract()
-    # rename_folders()
