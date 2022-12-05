4b3cf898e5fed4336058eae0693a3c84aac957f6
Adheesh Shenoy <adheeshenoy@gmail.com>
2022-12-05 03:30:51 -0800

    Added Baseline overfit model and mec_model

--- data_measurements.py
+++ data_measurements.py
@@ -19,7 +19,15 @@ def compute_mec(filename = 'encoded_data.csv', image_h = 224, image_w = 224, ima
     min_thresh = log2(threshold + 1)
     
     return (min_thresh * (image_h * image_w * image_c) + 1) + (min_thresh + 1)
+
+# def compute_mec_2(image_h = 224, image_w = 224, image_c = 3, cls = 1000):
+#     return log2(((image_h * image_w * image_c) + 1) * 1000) * df.shape[0]
     
+def capacity_progression(filename = 'encoded_data.csv'):
+    df = pd.read_csv(filename, header = None)
+    df = df.groupby(df.columns[-1]).head(5)
+    print(df)
 
 if __name__ == '__main__':
-    print('mec = ', compute_mec())
+    # print('mec = ', compute_mec())
+    capacity_progression()
