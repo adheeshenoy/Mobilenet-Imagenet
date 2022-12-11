5658f2f0217af1321b7a58e2cb79aa7823fe01a3
Adheesh Shenoy <adheeshenoy@gmail.com>
2022-12-10 17:33:14 -0800

    Added MEC code

--- data_measurements.py
+++ data_measurements.py
@@ -1,33 +1,35 @@
 import pandas as pd
-from math import log2
+from math import log2, log
 import tqdm
 
-def compute_mec(filename = 'encoded_data.csv', image_h = 224, image_w = 224, image_c = 3):
+def compute_mec(filename = 'encoded_data.csv', classes = 1000):
+    '''
+    32 -> 41024
+    64 -> 82
+    
+    '''
     df = pd.read_csv(filename, header = None)
-    df['labels'] = df[df.columns[-1]]
+    latent_dimension = df.shape[1] - 1
+    
+    print('latent space dimension:', latent_dimension)
+    
     df['sums'] = df[df.columns[:-1]].sum(axis = 1)
+    df['labels'] = df[df.columns[-2]]
     df = df[['labels', 'sums']].sort_values(by = 'sums')
     print(df.head())
 
     threshold = 0
     cls = None
-    for i, row in tqdm.tqdm(df.iterrows()):
+    s = None
+    for _, row in tqdm.tqdm(df.iterrows()):
         if row['labels'] != cls:
+            threshold += 2 if row['sums'] != s else 1
             cls = row['labels']
-            threshold += 1
+            s = row['sums']
 
     min_thresh = log2(threshold + 1)
     
-    return (min_thresh * (image_h * image_w * image_c) + 1) + (min_thresh + 1)
-
-# def compute_mec_2(image_h = 224, image_w = 224, image_c = 3, cls = 1000):
-#     return log2(((image_h * image_w * image_c) + 1) * 1000) * df.shape[0]
-    
-def capacity_progression(filename = 'encoded_data.csv'):
-    df = pd.read_csv(filename, header = None)
-    df = df.groupby(df.columns[-1]).head(5)
-    print(df)
+    return (min_thresh * (latent_dimension + 1)) + ((min_thresh + 1) * classes)
 
 if __name__ == '__main__':
-    # print('mec = ', compute_mec())
-    capacity_progression()
+    print('mec = ', compute_mec())
